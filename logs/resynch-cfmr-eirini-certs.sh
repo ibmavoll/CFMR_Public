1a32b71378ddfc2f6a1a42258ab2453d849a0bc0
Sanjay Joshi <joshisa@us.ibm.com>
2021-05-19 00:59:04 -0400

    Updates to eirini cert rebuild guidance and helper script

--- logs/resynch-cfmr-eirini-certs.sh
+++ logs/resynch-cfmr-eirini-certs.sh
@@ -1,18 +1,3 @@
-#### Background
-As a result of a cluster restart, certain CFMR pods dynamically regenerate assets such as mutatingwebhookconfigurations and secrets.  Unfortunately, if a cluster is shutdown - the pods restart - however the existing assets are not overwritten.  This results in a mismatched (out-of-synch) condition.  We'll be opening an issue with dev to improve this experience.  We think linking the ownership of these resources to their parent owning deploy or pod could cause the K8s GC to cleanup these out-of-synch resources during cluster shutdown automatically.  
-
-#### Decreasing the sensitivity to warnings on the mutatingwebhookconfiguration
-
-```
-oc -n cfmr patch mutatingwebhookconfiguration eirini-persi-mutating-hook --type=JSON -p '[{"op":"replace","path":"/webhooks/0/failurePolicy","value":"Ignore"}]'
-```
-
-Reference:  [https://kubernetes.io/docs/reference/access-authn-authz/extensible-admission-controllers/#failure-policy](https://kubernetes.io/docs/reference/access-authn-authz/extensible-admission-controllers/#failure-policy)
-
-
-#### Re-synchronize (regenerate) the webhook certificates, keys and CAs and the setup secret with the `persi` deployment (running cluster)
-
-```
 #!/bin/bash
 # ----------------------------------------------------------------------------------------------------\\
 # Description:
@@ -58,6 +43,10 @@ oc -n cfmr scale deploy ssh --replicas=1
 oc -n cfmr scale deploy instance-index-env-injector --replicas=1
 oc -n cfmr scale deploy eirini-annotate-extension --replicas=1
 
+echo "Sleeping for 7 seconds to let new certs and webhook configurations to instantiate"
+sleep 7
+
+echo "Begin validation ..."
 # Validate
 oc get mutatingwebhookconfiguration eirini-persi-mutating-hook
 oc get mutatingwebhookconfigurations eirini-dns-aliases-mutating-hook
@@ -71,35 +60,19 @@ oc -n cfmr get secret eirini-ssh-setupcertificate
 oc -n cfmr get secret eirini-x-setupcertificate
 oc -n cfmr get secret eirinix-annotation-setupcertificate
 
-# echo "Persi Logs ..."
+echo ""
+echo "Persi Logs ..."
 oc -n cfmr logs -l app.kubernetes.io/component=persi
-
-# echo "Eirini DNS Aliases Logs ..."
+echo ""
+echo "Eirini DNS Aliases Logs ..."
 oc -n cfmr logs -l app.kubernetes.io/component=eirini-dns-aliases
-
-# echo "Eirini SSH Logs ..."
+echo ""
+echo "Eirini SSH Logs ..."
 oc -n cfmr logs -l app.kubernetes.io/component=ssh
-
-# echo "Eirini Instance Index Env Injector Logs ..."
+echo ""
+echo "Eirini Instance Index Env Injector Logs ..."
 oc -n cfmr logs -l app.kubernetes.io/component=instance-index-env-injector
-
-# echo "Eirini Annotate Logs ..."
+echo ""
+echo "Eirini Annotate Logs ..."
 oc -n cfmr logs -l app.kubernetes.io/name=eirini-annotate-extension
-```
-
-#### Troubleshooting tips
-Example Logs from Eirini-Persi Pod
-
-```
-▶ oc logs persi-6ff46f54cd-z6knj
-2021-05-03T16:25:54.303Z	INFO	internal/start.go:41	Starting v0.0.0-dirty+76.g6b229dd with namespace cfmr-eirini
-{"level":"info","ts":1620059154.3038177,"caller":"kubeconfig/getter.go:53","msg":"Using in-cluster kube config"}
-{"level":"info","ts":1620059154.3038573,"caller":"kubeconfig/checker.go:36","msg":"Checking kube config"}
-{"level":"info","ts":1620059154.30802,"caller":"kubeconfig/getter.go:53","msg":"Using in-cluster kube config"}
-{"level":"info","ts":1620059154.3081286,"caller":"kubeconfig/checker.go:36","msg":"Checking kube config"}
-{"level":"info","ts":1620059154.9382117,"caller":"ctxlog/context.go:51","msg":"Creating webhook server certificate"}
-```
-
-Symptom
-
-![](webhook-out-of-synch-symptom.png)
+echo ""
